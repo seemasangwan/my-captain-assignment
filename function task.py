fe451a969763fbf53b79905b4953a45e90d666fc
seemasangwan <104920728+seemasangwan@users.noreply.github.com>
2022-05-22 10:15:27 +0530

    Update function task.py

--- function task.py
+++ function task.py
@@ -1,14 +1,1 @@
-import operator
-def most_frequent(string):
-  d={}
-  for key in string :
-     if key not in d:
-        d[key]=1
-     else:
-       d[key]=d[key]+1
-  des=dict(sorted(d.items(),key=operator.itemgetter(1)))
-  print("decreasing order",des)
-  
 
-string=str(input("please enter a string"))
-print(most_frequent(string))
